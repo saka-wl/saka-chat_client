e672507670ec2baf841fb3d3945a3991d0e19cfc
“wangli” <wangli@kkworld.com>
2024-08-07 19:07:03 +0800

    feat: 修改聊天好友列表中pinia的全局变量

---
+++ src/component/Layout/LayoutTwoColumn/index.vue
@@ -0,0 +1,30 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="layout-two-column-container">
+        <div class="left">
+            <!-- <MineList /> -->
+            <slot></slot>
+        </div>
+        <div class="right">
+            <router-view></router-view>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "src/assets/style/common.scss";
+.layout-two-column-container {
+    display: flex;
+    height: 100%;
+    .left {
+        width: px2vw(300);
+        height: 100%;
+    }
+    .right {
+        flex: 1 1 auto;
+    }
+}
+</style>
---
+++ src/view/Chat/component/ChatRoomContent.vue
@@ -0,0 +1,20 @@
+<script lang="ts" setup>
+import { useRoute } from 'vue-router';
+import ChatMessageInfoItem from './ChatMessageInfoItem.vue';
+
+const route = useRoute();
+
+const { friendAvatar } = route.params as { friendAvatar: string };
+
+</script>
+
+<template>
+    <div class="chatroom-content-container">
+        <ChatMessageInfoItem type="left" message="11111" :avatar="friendAvatar" color="" />
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "src/assets/style/common.scss";
+
+</style>
